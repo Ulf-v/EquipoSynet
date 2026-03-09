33bf8538e029b087493052b590efc86ec964a0e2
alvaroyalonso <amolinaduenas@gmail.com>
2026-03-09 20:33:27 +0100

    Borrado de clases innecesarias y prueba del login con google oauth2

--- Backend-springboot/src/main/java/com/synet/demoBack/config/MongoConfig.java
+++ Backend-springboot/src/main/java/com/synet/demoBack/config/MongoConfig.java
@@ -1,4 +1,4 @@
-package com.tuempresa.tuproyecto.config;
+package com.synet.demoBack.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
