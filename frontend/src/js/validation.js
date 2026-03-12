6c20717a0b7adf741d5e7f29984d3e84698cef99
Alanferg <lvarofdez@gmail.com>
2026-03-12 11:50:38 +0100

    Mostrar los casos aleatorios en la página de validación

--- frontend/src/js/validation.js
+++ frontend/src/js/validation.js
@@ -1,8 +1,55 @@
+const edad = document.getElementById("edad");
+const sexo = document.getElementById("sexo");
+const antecedentes = document.getElementById("antecedentes");
+const antecedentesQuir = document.getElementById("antecedentesQuir");
+const habitos = document.getElementById("habitos");
+const situacionBas = document.getElementById("situiacionBas");
+const medicacion = document.getElementById("medicacion");
+const antecedentesFam = document.getElementById("antecedentesFam");
+const motivo = document.getElementById("motivo");
+const sintomas = document.getElementById("sintomas");
+const exploracion = document.getElementById("exploracion");
+const signos = document.getElementById("signos");
+const resultados = document.getElementById("resultados");
+const razonamiento = document.getElementById("razonamiento");
+const diagnosticoFinal = document.getElementById("diagnósticoFinal");
+const tratamientoFarma = document.getElementById("tratamientoFarma");
+const tratamientoNoFarma = document.getElementById("tratamientoNoFarma");
+const factores = document.getElementById("factores");
+const alergias = document.getElementById("alergias");
+const referencias = document.getElementById("referencias");
 
-const mostrarCaso = async () => {
+const cargarCasos = async () => {
     const res = await fetch("https://synet.riberadeltajo.es/api/archivos");
     const resJson = await res.json();
-    console.log(resJson);
+
+    mostrarCaso(resJson);
 }
 
-document.addEventListener("DOMContentLoaded", mostrarCaso);
+const mostrarCaso = (casosJson) => {
+    const rand = Math.floor(Math.random() * casosJson.length);
+    const caso = casosJson[rand];
+
+    edad.textContent = caso.edad;
+    sexo.textContent = caso.sexo;
+    antecedentes.textContent = caso.antecedentes_medicos;
+    antecedentesQuir.textContent = caso.antecedentes_quirurgicos;
+    habitos.textContent = caso.habitos;
+    situacionBas.textContent = caso.situacion_basal;
+    medicacion.textContent = caso.medicacion_actual;
+    antecedentesFam.textContent = caso.antecedentes_familiares;
+    motivo.textContent = caso.motivo;
+    sintomas.textContent = caso.sintomas;
+    exploracion.textContent = caso.exploracion_general;
+    signos.textContent = caso.signos;
+    resultados.textContent = caso.resultados_pruebas;
+    razonamiento.textContent = caso.razonamiento_clinico;
+    diagnosticoFinal.textContent = caso.diagnostico_final;
+    tratamientoFarma.textContent = caso.tratamiento_farmacologico;
+    tratamientoNoFarma.textContent = caso.tratamiento_no_farmacologico;
+    factores.textContent = caso.factores_sociales;
+    alergias.textContent = caso.alergias;
+    referencias.textContent = caso.referencias_bibliograficas;
+}
+
+document.addEventListener("DOMContentLoaded", cargarCasos);
